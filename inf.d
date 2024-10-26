6e28efdb346537eedd95790e7dbc9e0e10d1109d
JohnZone <kimmonorusha@gmail.com>
2024-10-26 18:54:07 +0300

    Create inf.d

---
+++ inf.d
@@ -0,0 +1,576 @@
+Step 1: A1Z26
+A1Z26
+
+The A1Z26 code is a very simple code. As you know there are 26 letters in the American alphabet so Z would equal 26 because it is the 26th letter in the alphabet.
+
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+
+Step 2: ASCII
+ASCII
+ASCII
+
+ASCII is a computer code that is similar to binary. Instead of using 1's and 0's like binary it uses the numbers from 1-256
+
+A=65
+
+B=66
+
+C=67
+
+D=68
+
+E=69
+
+F=70
+
+G=71
+
+H=72
+
+I=73
+
+J=74
+
+K=75
+
+L=76
+
+M=77
+
+N=78
+
+O=79
+
+P=80
+
+Q=81
+
+R=82
+
+S=83
+
+T=84
+
+U=85
+
+V=86
+
+W=87
+
+X=88
+
+Y=89
+
+Z=90
+
+a=97
+
+b=98
+
+c=99
+
+d=100
+
+e=101
+
+f=102
+
+g=103
+
+h=104
+
+i=105
+
+j=106
+
+k=107
+
+l=108
+
+m=109
+
+n=110
+
+o=111
+
+p=112
+
+q=113
+
+r=114
+
+s=115
+
+t=116
+
+u=117
+
+v=118
+
+w=119
+
+x=120
+
+y=121
+
+z=122
+
+0=48
+
+1=49
+
+2=50
+
+3=51
+
+4=52
+
+5=53
+
+6=54
+
+7=55
+
+8=56
+
+9=57
+
+Step 3: Atbash
+Atbash
+Atbash
+
+The Atbash code is just the alphabet backwards. For example A would equal Z.
+
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+Z Y X W V U T S R Q P O N M L K J I H G F E D C B A
+
+Step 4: Binary
+Binary
+
+The Binary code is a code that the computers recognize using only 1's and 0's. It is a very complicated code because they are different for lowercase and capital.
+
+A 01000001
+
+B 01000010
+
+C 01000011
+
+D 01000100
+
+E 01000101
+
+F 01000110
+
+G 01000111
+
+H 01001000
+
+I 01001001
+
+J 01001010
+
+K 01001011
+
+L 01001100
+
+M 01001101
+
+N 01001110
+
+O 01001111
+
+P 01010000
+
+Q 01010001
+
+R 01010010
+
+S 01010011
+
+T 01010100
+
+U 01010101
+
+V 01010110
+
+W 01010111
+
+X 01011000
+
+Y 01011001
+
+Z 01011010
+
+a 01100001
+
+b 01100010
+
+c 01100011
+
+d 01100100
+
+e 01100101
+
+f 01100110
+
+g 01100111
+
+h 01101000
+
+i 01101001
+
+j 01101010
+
+k 01101011
+
+l 01101100
+
+m 01101101
+
+n 01101110
+
+o 01101111
+
+p 01110000
+
+q 01110001
+
+r 01110010
+
+s 01110011
+
+t 01110100
+
+u 01110101
+
+v 01110110
+
+w 01110111
+
+x 01111000
+
+y 01111001
+
+z 01111010
+
+0. 00110000
+
+1. 00110001
+
+2. 00110010
+
+3. 00110011
+
+4. 00110100
+
+5. 00110101
+
+6. 00110110
+
+7. 00110111
+
+8. 00111000
+
+9. 00111001
+
+! 00100001
+@ 01000000
+
+# 00100011
+
+$ 00100100
+
+% 00100101
+
+^ 01011110
+
+& 00100110
+
+* 00101010
+
+( 00101000
+
+) 00101001
+
+- 00101101
+
+_ 01011111
+
++ 00101011
+
+= 00111101
+
+; 00111011
+
+: 00111010
+
+" 00100010
+
+' 00100111
+
+` 01100000
+
+~ 01111110
+
+< 00111100
+
+> 00111110
+
+? 00111111
+
+¿ 10111111
+
+, 00101100
+
+. 00101110
+
+/ 00101111
+
+{ 01111011
+
+} 01111101
+
+[ 01011011
+
+] 01011101
+
+€ 10101100
+
+¶ 10110110
+
+Step 5: Braille
+Braille
+Braille
+
+Braille is what blind people read. It is a series of raised dots that tell you what letter, number, or word it is.
+
+Step 6: Caesar Cipher
+Caesar Cipher
+
+The Caesar cipher is a code Julius Caesar invented when he mailed letters. He invented it so if his messenger was robbed of that letter and the robber wouldn't be able to read it. It is probably one of the most simple codes ever. It is 3 letters back so A would be X. The Rot Cipher is almost the same as the Caesar Cipher.
+
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+X Y Z A B C D E F G H I J K L M N O P Q R S T U V W
+
+Step 7: Columnar Cipher
+Columnar Cipher
+
+The Columnar Cipher is a type of transposition cipher.
+
+1. Get Keyword
+
+2. If your keyword is Zebras, that is 6 letters. You would write 632415 because Z is the 6th highest letter in the word and E is the 3rd highest letter and so on
+
+3. Then message under the numbers in rows of 6, because Zebra is a 6 letter word.
+
+4. Write out 123456. Under the number write the letters from each column that match the numbers in the original line of numbers.
+
+Step 8: Combination Cipher
+Combination Cipher
+
+A Combination Cipher is a Cipher using 2 or more codes. For example if you wanted to make the best code ever, you could do Atbash, Caesar Cipher, Vigernere Cipher, and then A1Z26.
+
+Step 9: Dice Cipher
+Dice Cipher
+
+The Dice Cipher is not dice, it's just squares with dots in certain places.
+
+Step 10: Digraph Cipher
+Digraph Cipher
+Digraph Cipher
+
+The Digraph Cipher is kind of like the Vigenere Cipher. When you write a sentence you would write it in pairs of twos, li ke th is, and if there is a letter left over add an x to it. The pairs of letters will be the coordinates for the two letters. An example would be "IA MT RY IN GT OE XP LA IN TH IS" = "VG ZN EI VT TN BC KR YG VT GZ VO". To decipher it the decoded letters will be the coordinates.
+
+Step 11: Dorabella Cipher
+Dorabella Cipher
+Dorabella Cipher
+
+The Dorabella was made by Edward Elgar on July 14, 1897 for his young friend Dora Penny. It is not confirmed a solved code.
+
+Step 12: Francis Bacons Substitution Cipher
+Francis Bacons Substitution Cipher
+
+One of Bacons best code was a code that used bold and regular fonts in a certain order to make a new letter. For example "code" would be something like this "FrancisBacon was a cool guy". After you see the sentence put all of the letters in to groups of 5, like this "Franc isBac onwas acool" leave out any extra letters. Once you have it like this you are ready to decode.
+
+*= Regular Letter B= Bold Letter
+
+A= *****
+
+B=****B
+
+C=***B*
+
+D=***BB
+
+E=**B**
+
+F=**B*B
+
+G=**BB*
+
+H=**BBB
+
+I=*B***
+
+J=*B**B
+
+K=*B*B*
+
+L=*B*BB
+
+M=*BB**
+
+N=*BB*B
+
+O=*BBB*
+
+P=*BBBB
+
+Q=B****
+
+R=B***B
+
+S=B**B*
+
+T=B**BB
+
+U=B*B**
+
+V=B*B*B
+
+W=B*BB*
+
+X=B*BBB
+
+Y=BB***
+
+Z=BB**B
+
+Step 13: Grid Transposition Cipher
+Grid Transposition Cipher
+Grid Transposition Cipher
+
+The Grid Transposition cipher is just another type of transposition cipher. First make a grid that can fit all of the letters, you can do that by taking the square root of the total number of letters, if it comes out as a decimals round up. If there are extra spaces add X's. Then you scatter the numbers in a random order. Then Match the coordinates onto the second grid. Best codes would come out as EDSEBSCTO. To decode it all you need to do is make another grid with the letters in the correct order. Finally Match the coordinates onto the correct grid.
+
+Step 14: Keyboard Code
+Keyboard Code
+
+The Keyboard Code is just the order of letters your keyboard.
+
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+Q W E R T Y U I O P A S D F G H J K L Z X C V B N M
+
+Step 15: Morse Code
+Morse Code
+Morse Code
+
+Morse code was invented by Samuel Morse and was used in the early 1800's to message people in a telegram. It is a series of beeps that are short and long. For example _ is called a dash and it would be a long beep, and . would be dot and it would be a short beep.
+
+Step 16: Phone Code
+Phone Code
+
+The Phone code is really cool because not a lot of people know it. It is just the number the letter is on and then what number it is on that number. For example A is on 1st number on 2 so it would be 2 1
+
+2 2 2 3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 7 8 8 8 9 9 9 9
+
+1 2 3 1 2 3 1 2 3 1 2 3 1 2 3 1 2 3 4 1 2 3 1 2 3 4
+
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+Step 17: Pigpen Cipher
+Pigpen Cipher
+Pigpen Cipher
+
+The Pigpen Cipher was created by the Freemasons so they could keep documents safe. It was also used by the confederate soldiers during the Civil War. It is called The Pigpen Cipher because the box's look like pigpens and the dots look like pigs. It seems complicated but it isn't really. The lines surrounding the letter and the dots within those lines are the symbols.
+
+Step 18: Rosicrucian Cipher
+Rosicrucian Cipher
+
+The Rosicrucian Cipher is almost exactly like the the Pigpen Cipher. The symbol that the letter is inside is the symbol that you put for that letter.
+
+Step 19: ROT Cipher
+ROT Cipher
+Rot Cipher
+
+The Rot Cipher is when you take a letter and put it back or fourth to equal a different letter. An example of this would be -1 equals A=Z. +1 equals A+B It is Related to the Caesar Cipher.
+
+This is a Print out of the Rot Cipher Wheel. Cut out the 2 circles leaving the inner circle NOT HOLLOW. Then you stick a tack or a paper clip through the middle of the inner wheel. Then you can spin it around to do your cipher.
+
+Step 20: Rout Cipher
+Rout Cipher
+Rout Cipher
+
+The Rout Cipher is your message in a patter kind of like a word search. You make an arrow in the direction of the first two or three letters and then leave it to the other person to do the rest. To make is easier you can make an arrow all the way through.
+
+Step 21: Scytale
+Scytale
+Scytale
+
+The cylinder decodes for you!
+
+To make a cylinder cipher you need long strip of paper and cylinder.
+
+Wrap the paper around the cylinder so there are no parts of the cylinder showing.
+
+You can temporarily tape down the edges to help you with this part. write your message across the cylinder (write 1 or 2 letters on each part of the strip) Then unwrap the paper from the cylinder.
+
+You can't decode the cipher unless you have a cylinder the same diameter as the one it was made on.
+
+Step 22: Tap Codes
+Tap Codes
+Tap Code
+
+The Tap Code was used by Vietnam prisoners to communicate, usually by tapping on metal bars or walls. It is a combination of Morse Code and the Grid Code replacing K with a C or X. the dots (.) tell you what numbers to go to from top to side, and the dash's (/) tell you when to separate a word.
+
+Step 23: The Grid Code
+The Grid Code
+The Grid Code
+
+The Grid Code is a 5x5 grid. It is very simple.
+
+Step 24: Transposition Cipher
+Transposition Cipher
+Transposition Cipher
+
+Transposition Ciphers can be words written backwards for example (you are cool)=(uoy era looc). It could also be every pair or every 3 letters a pair of letters are swapped. An example of that would be (you are cool)=(oya uer oclo)
+
+Step 25: Vigenere Cipher
+
+Vigenere Cipher
+Vigenere Cipher
+
+The Vigenere Cipher is a 26x26 grid of letters a-z. It is a more complicated cipher so I will have to try to demonstrate with explaining battle ship.
+
+Directions
+
+1. Choose a code word (any word. no numbers)
+
+2. Write it like this (my code word will be "code") i m t r y i n g t o e x p l a i n h o w t o d o t h i s it doesn't matter if the code word cant fit evenly c o d e c o d e c o d e c o d e c o d e c o d e c o d e
+
+3. Look at the grid and follow the row i and the column c to the intersection like in battle ship. the letter they intersect on is k. that is how you encode it. To decode it take the code letter in this case c and go until you find k. Then go up all the way so you will find i.
+
+Step 26: Wig Wag
+Wig Wag
+Wig Wag
+
+Wig Wag was used in the civil war to communicate during battles. It is pretty easy to do, you just have to remember that you don't have to write out all of some words.
+
+Step 27: Subscribe to Bolillo Kremer
+
+If you enjoyed this tutorial or just enjoyed any part of this, please subscribe to Bolillo Kremer.
+
+I will be making more funny robot projects very soon and I would really appreciate your support! Thank you so much! I'll see ya on the next tutorial ;)
+
+
+Participant in:
+Apocalypse Preparedness Contest
+View Contest
+Be the First to Share
+Did you make this project? Share it with us!
+
+I Made It
